a7621d5c38e179bb854450dd17174ab92435ee55
ryan <c.ryanperkins@gmail.com>
2014-10-02 16:42:16 -0700

    restaurant app

---
+++ RestaurantApp/restaurantapp.js
@@ -0,0 +1,28 @@
+/**
+ * Created by Ryan and Shalonda on 10/2/14.
+ */
+var check_total;
+check_total = prompt('What is the total bill?');
+ alert(check_total);
+
+var total_diners;
+total_diners = prompt('How many diners are there?');
+    alert(total_diners);
+
+var tip;
+tip = check_total * .15;
+    alert(tip);
+
+
+
+/*var total;
+total = tip + check_total;
+    alert(total);
+
+var itemized_bill = function(a, b){
+    if(total_diners < 6){
+        return a / b;
+    }
+};
+*/
+
